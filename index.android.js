a02c3029886de51a2e8eae4dfec780ceff5a701a
Sheena Nickerson <sheenanick@gmail.com>
2017-03-12 17:02:23 -0700

    Add album list component with API call to get album data

--- index.android.js
+++ index.android.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
-import Header from './src/components/header';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 const App = () => (
-  <Header headerText='Albums'/>
+  <View>
+    <Header headerText='Albums'/>
+    <AlbumList />
+  </View>
 );
 
 //name of string must match project name
