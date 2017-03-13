0c632282b32b3140e669849f260ffec24d48ecd6
Sheena Nickerson <sheenanick@gmail.com>
2017-03-12 18:13:08 -0700

    Add card section component and display albums in scroll view

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,13 +1,58 @@
 import React from 'react';
-import { View, Text} from 'react-native';
+import { View, Text, Image } from 'react-native';
 import Card from './Card'
+import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({album}) => {
+  const { title, artist, thumbnail_image, image } = album;
+  const {
+    thumbnailStyle,
+    thumbnailContainerStyle,
+    headerContentStyle,
+    headerTextStyle,
+    albumImageStyle
+  } = styles;
+
   return (
     <Card>
-      <Text>{props.album.title}</Text>
+      <CardSection>
+        <View style={thumbnailContainerStyle}>
+          <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
+        </View>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image style={albumImageStyle} source={{ uri: image }} />
+      </CardSection>
     </Card>
   );
 };
+
+const styles = {
+  thumbnailContainerStyle: {
+    marginRight: 10,
+    marginLeft: 10,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  headerContentStyle: {
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 20
+  },
+  albumImageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
+}
 
 export default AlbumDetail;
